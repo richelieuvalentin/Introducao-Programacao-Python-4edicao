002118cf4230a1d6331cbea5e41f26375c572842
Richelieu Valentin <richelieuvalentin@gmail.com>
2025-07-29 22:44:49 -0300

    Primeiros scripts de listas do capítulo 6

---
+++ Cap06/Programa.6.5.py
@@ -0,0 +1,8 @@
+# Programa 6.5: Repetição com tamanho da lista usando len
+
+L = [1, 2, 3]
+x = 0
+
+while x < len(L):
+    print(L[x])
+    x += 1
---
+++ Cap06/Programa.6.6.py
@@ -0,0 +1,16 @@
+# Programa 6.6: Adição de elementos à lista
+
+L = []
+
+while True:
+    n = int(input("Digite um número (0 sai): "))
+    if n == 0:
+        break
+    L.append(n)
+
+x = 0
+
+while x < len(L):
+    print(L[x])
+    x += 1
+    
